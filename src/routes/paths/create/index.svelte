5f96f05cf46f8f37629dfa5be169fa5cf2b72c40
Angel <santoyox714@gmail.com>
2022-06-11 20:00:57 -0700

    /paths/create changes

--- src/routes/paths/create/index.svelte
+++ src/routes/paths/create/index.svelte
@@ -1,28 +1,57 @@
-
 <script>
   export let photos;
 </script>
 
-<h1> Photos </h1>
+<h1>Photos</h1>
+
+<form method= "post" enctype ="multipart/form-data">
+
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <div class="grid">
   {#each photos as photo}
     <a sveltekit:prefetch href="/photos/{photo.id}">
-      <img src={photo.thumbnailUrl} alt={photo.title}>
+      <img src={photo.thumbnailUrl} alt="{photo.title} thumbnail" />
     </a>
-    {/each}
+  {/each}
 </div>
 
 <style>
-	.gri {
-		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-gap: 8px;
-	}
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 0.5em;
+  }
 
-	figure, img {
-		width: 100%;
-		margin: 0;
-	}
+  a,
+  img {
+    display: block;
+    width: 100%;
+  }
+  form {
+    display: flex;
+    width: 100%;
+  }
 </style>
--- src/routes/paths/create/index.ts
+++ src/routes/paths/create/index.ts
@@ -1,4 +1,16 @@
 
+
+export async function get({ params }){
+
+  const res= await fetch('https://jsonplaceholder.typicode.com/photos')
+  const photos = await res.json();
+  
+  return {
+    body:{
+      photos: photos.slice(0,100)
+    }
+  };
+  } 
 export async function post ({request})
 {
   const data= await request.formData();
